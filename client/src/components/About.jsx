9258107ae2559fa25bd90b1693c1eedfcd91efc4
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-04 12:00:49 +0530

    Create About.jsx

---
+++ client/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Marketing Strategy Test App</h2>
+            <p>Learn more about how our app can help analyze your marketing campaigns.</p>
+        </div>
+    );
+};
+
+export default About;
