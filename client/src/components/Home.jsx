7829d687cbd046749d652ad44f2dbcc02af90650
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-04 12:00:09 +0530

    Create Home.jsx

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Marketing Strategy Test App</h1>
+            <p>Discover the success of your campaigns with our intuitive tool!</p>
+        </div>
+    );
+};
+
+export default Home;
