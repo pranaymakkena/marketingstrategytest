8a428c4d554a171e6c2a42e4f154e9bcfd813aa3
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-04 12:01:16 +0530

    Create Success.jsx

---
+++ client/src/components/Success.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Success = () => {
+    return (
+        <div>
+            <h2>Congratulations!</h2>
+            <p>Your campaign was successful.</p>
+        </div>
+    );
+};
+
+export default Success;
